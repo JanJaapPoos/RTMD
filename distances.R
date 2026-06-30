0725ab83a1d3726dcb3b187959c34ca091c934d7
JanJaapPoos <janjaap.poos@wur.nl>
2026-06-30 14:35:33 +0200

    Delete distances.R

--- distances.R
+++
@@ -1,4 +0,0 @@
-distances <- function (shapes) {
-   cat("Distances between snout and caudal fin landmarks in ",shapes$unit[1]," \n") 
-   print(apply(shapes$landmarks[c("snout", "caudal_fin"), , ], 3, distancePointToPoint ))
-}
